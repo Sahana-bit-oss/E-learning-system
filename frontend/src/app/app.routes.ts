ca58cdf52412ca888d2049f8685d203e98bb8325
Adarsh Patil <adarshrpatil02@gmail.com>
2024-10-23 00:19:49 +0530

    removed professor reg and login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,31 +1,23 @@
-import { Route, Router, Routes } from "@angular/router";
+import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { RegisterComponent } from "./register/register.component";
 import { LoginComponent } from "./login/login.component";
 import { AdminRegisterComponent } from "./admin-register/admin-register.component";
-import { ProfessorRegisterComponent } from "./professor-register/professor-register.component";
 import { StudentRegisterComponent } from "./student-register/student-register.component";
 import { ContactComponent } from "./contact/contact.component";
 
-
-export const routes:Routes=[
-    {path:'home',component:HomeComponent},
-    {path:'home/about',component:AboutComponent},
-    {path:'home/contact',component:ContactComponent},
-    {path:'home/register',component:RegisterComponent},
-    {path:'home/login',component:LoginComponent},
-    {path:'home/register/admin-register',component:AdminRegisterComponent},
-    {path:'home/register/professor-register',component:ProfessorRegisterComponent},
-    {path:'home/register/student-register',component:StudentRegisterComponent},
-    {path:'home/login/register',component:RegisterComponent},
-    {path:'home/register/admin-register/login',component:LoginComponent},
-    {path:'home/register/professor-register/login',component:LoginComponent},
-    {path:'home/register/student-register/login',component:LoginComponent}
-]
-
-
-
-
-
-
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'home/about', component: AboutComponent },
+    { path: 'home/contact', component: ContactComponent },
+    { path: 'home/register', component: RegisterComponent },
+    { path: 'home/login', component: LoginComponent },
+    { path: 'home/register/admin-register', component: AdminRegisterComponent },
+    { path: 'home/register/student-register', component: StudentRegisterComponent },
+    { path: 'home/login/register', component: RegisterComponent },
+    { path: 'home/register/admin-register/login', component: LoginComponent },
+    { path: 'home/register/professor-register/login', component: LoginComponent },
+    { path: 'home/register/student-register/login', component: LoginComponent }
+];
